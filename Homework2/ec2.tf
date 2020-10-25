bc6e84dc8f61eb7dec8cb2f2a075974f8741aa19
moshe.even <moshe.even@imperva.com>
2020-10-25 20:45:06 +0200

    add data object for the ami id

--- Homework2/ec2.tf
+++ Homework2/ec2.tf
@@ -1,8 +1,22 @@
+data "aws_ami" "ubuntu-18_04_with_nginx" {
+    most_recent = true
+    owners = ["455198789788"]
+
+    filter {
+        name   = "name"
+        values = ["nginx"]
+    }
+    filter {
+        name   = "architecture"
+        values = ["x86_64"]
+    }
+}
+
 # web servers
 resource "aws_instance" "web_server" {
     count = var.ec2_count
     # ami = data.aws_ami.ubuntu-18_04.id
-    ami ="ami-0aca7bcb5f6e3ec20"
+    ami = data.aws_ami.ubuntu-18_04_with_nginx.id # this ami is with nginx installed
     instance_type = "t2.micro"
     key_name = var.key_name
     vpc_security_group_ids = [aws_security_group.allow_ngix.id]
